6007106a2dfa4b5eef343767eb4449d092fbd1e0
alyx <pokepim@zoho.eu>
2020-09-05 10:09:17 +0200

    ready for deployment

--- src/store/actions.js
+++ src/store/actions.js
@@ -2,7 +2,7 @@ import Api from "../service/api";
 
 export default {
   async loadData({ commit }, stock) {
-    const response = await Api().get(`/${stock}`, stock);
+    const response = await Api().get(`/stock/${stock}`, stock);
     commit("updateData", response.data);
   },
   resetData({ commit }, stock) {
